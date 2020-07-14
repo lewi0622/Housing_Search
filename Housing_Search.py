a8ab6ba0df052a5b467e43a793f63943b30f5f97
Eric Lewiston <eric.lewiston.el@gmail.com>
2020-07-14 13:10:17 -0500

    added apartments.com seach and function

--- Housing_Search.py
+++ Housing_Search.py
@@ -44,7 +44,7 @@ def get_zillow(url):
                 print(".", end="")
                 continue
         break
-    print('\n')
+    print()
 
     # Check title for '0 Rentals'. Zillow gives 'similar results nearby' in this case which we ignore
     zero_rentals = False
@@ -80,20 +80,35 @@ def get_craigslist(url):
             log.writelines(debug_msg)
         return ['check error log' + str(pd.to_datetime('today'))]
 
-    return [listing['href'] for listing in criagslist_soup.find_all('a', attrs={'class': 'result-image gallery'})]
+    try:
+        return [listing['href'] for listing in criagslist_soup.find_all('a', attrs={'class': 'result-image gallery'})]
+    except:
+        return []
 
+
+def get_apartments(url):
+    # Get Apartments.com urls
+    print("Trying to connect to Apartments.com")
+    apartments_soup = soups_on(url)
+
+    # print(apartments_soup.prettify())
+    
+    try:
+        return [listing['href'] for listing in apartments_soup.find_all('a', attrs={'class': 'placardTitle js-placardTitle'})]
+    except:
+        return []
 
 # Version Number
-version = 0.1
+version = 0.2
 debug = 0
 
 # Urls
 zillow_url = r'https://www.zillow.com/homes/for_rent/1-_beds/1.0-_baths/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22mapBounds%22%3A%7B%22west%22%3A-93.50802635404175%2C%22east%22%3A-93.10633873197143%2C%22south%22%3A44.868132495735146%2C%22north%22%3A45.062947488470165%7D%2C%22mapZoom%22%3A12%2C%22customRegionId%22%3A%222df575f09eX1-CR14087aax9bs72_uh1re%22%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22price%22%3A%7B%22min%22%3A282815%2C%22max%22%3A450470%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%2C%22baths%22%3A%7B%22min%22%3A1%7D%2C%22sqft%22%3A%7B%22min%22%3A700%7D%2C%22doz%22%3A%7B%22value%22%3A%221%22%7D%2C%22pmf%22%3A%7B%22value%22%3Afalse%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22min%22%3A1000%2C%22max%22%3A1700%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22cat%22%3A%7B%22value%22%3Atrue%7D%2C%22land%22%3A%7B%22value%22%3Afalse%7D%2C%22sdog%22%3A%7B%22value%22%3Atrue%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22pf%22%3A%7B%22value%22%3Afalse%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%7D%2C%22isListVisible%22%3Atrue%7D'
 criagslist_url = r'https://minneapolis.craigslist.org/search/apa?sort=date&hasPic=1&postedToday=1&bundleDuplicates=1&search_distance=4&postal=55402&min_price=1000&max_price=1700&minSqft=600&maxSqft=1400&availabilityMode=0&pets_cat=1&pets_dog=1&no_smoking=1&sale_date=all+dates'
-apts_url =r'https://www.apartments.com/houses/minneapolis-mn/min-1-bedrooms-1-bathrooms-1000-to-1700-pet-friendly/?bb=0gtmxlgn9K-t1tirG&so=8'
+apartments_url =r'https://www.apartments.com/houses/minneapolis-mn/min-1-bedrooms-1-bathrooms-1000-to-1700-pet-friendly/?bb=0gtmxlgn9K-t1tirG&so=8'
 
 # Main
-listings = get_zillow(zillow_url) + get_craigslist(criagslist_url)
+listings = get_zillow(zillow_url) + get_craigslist(criagslist_url) + get_apartments(apartments_url)
 
 # read csv
 csv_path = r"C:\Users\lewi0\Desktop\Personal_Scripts\Housing_Search\seen_listings.csv"
